8dbaefe6c42dbb0df7e2d38f2f8297972e0d87ff
Sagar Mitra <mitrasagar10@gmail.com>
2025-07-12 22:47:56 +0530

    env set up

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,12 +1,13 @@
 export const LOGIN_BACKGROUND_IMG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg";
 
-export const DEFAULT_PROFILE_PICTURE = "https://i.pinimg.com/736x/91/86/1b/91861b749841221d52122f0c2933d8a6.jpg"
+export const DEFAULT_PROFILE_PICTURE =
+  "https://i.pinimg.com/736x/91/86/1b/91861b749841221d52122f0c2933d8a6.jpg";
 
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWRjNzgyOWU0NTA0ZWZhMTYzZjA0YmEwMjNiMzkwMyIsIm5iZiI6MTc1MjMxODU5NC45NDEsInN1YiI6IjY4NzI0MjgyZWU0OThmMjEwNTZlYTVmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z0BC-Jm2BzWwydwRARUy9EPGy3YmeVlqCDhtwXEbjdo'
-  }
+    accept: "application/json",
+    Authorization: `Bearer  ${import.meta.env.VITE_TMDB_API_TOKEN}`,
+  },
 };
